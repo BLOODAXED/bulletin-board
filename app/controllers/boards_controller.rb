8aad52a3ebacb029dbd7f982682486ccef789b1d
Alex Slawniak <bloodaxed@gmail.com>
2025-09-24 01:38:34 +0000

    done

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,36 @@
 class BoardsController < ApplicationController
   def index
-    render({ :template => "board_templates/index"})
+    render({ :template => "board_templates/index" })
   end
+
   def show
-    render({ :template => "board_templates/show"})
+    render({ :template => "board_templates/show" })
+  end
+
+  def create
+    if params.fetch("name_param").empty?
+      redirect_to("/boards/")
+    else
+      @new_board = Board.new
+      @new_board.name = params.fetch("name_param")
+      @new_board.save
+      #render({ :template => "board_templates/create_confirm" })
+      redirect_to("boards/#{@new_board.id}")
+    end
+  end
+
+  def create_listing
+    if params.fetch("title").empty? or params.fetch("body").empty? or params.fetch("exp").empty?
+      redirect_to("/boards/#{params.fetch("board")}")
+    else
+      @new_listing = Listing.new
+      @new_listing.board_id = params.fetch("board")
+      @new_listing.title = params.fetch("title")
+      @new_listing.body = params.fetch("body")
+      @new_listing.expires_on = params.fetch("exp")
+      @new_listing.save
+      #render({ :template => "board_templates/create_confirm" })
+      redirect_to("/boards/#{params.fetch("board")}")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-  get("/", { :controller => "boards", :action => "index" })
+  get("/boards", { :controller => "boards", :action => "index" })
   get("/boards/:path_id", { :controller => "boards", :action => "show" })
+  post("/insert_board", { :controller => "boards", :action => "create" })
+  post("/insert_listing", { :controller => "boards", :action => "create_listing" })
 end
